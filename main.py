d3646dc2f81d79a5909e590b1dc13de015ad4415
Matheus Lazarin <mtslazarin@gmail.com>
2019-04-13 11:58:18 -0300

    comments insertion

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 13 11:41:05 2019
+
+@author: mtslazarin
+"""
+
+#%% Importando bibliotecas
+import pytta
+ 
+#%% Gerando sweep
+sweep = pytta.generate.sweep()
+
+#%% Tocando sweep
+sweep.play()
