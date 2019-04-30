4d2b68de65e6b4cf048eda61da82a38729481d45
Matheus Lazarin <mtslazarin@gmail.com>
2019-04-30 12:29:31 -0700

    Create pyttaco.py

---
+++ pyttaco.py
@@ -0,0 +1,29 @@
+# coding: -*- utf-8 -*-
+
+# Parte 1
+# Introdução  ao Python
+
+# 1.1 Data types
+
+# 1.2 Loops e condicionais
+
+# 1.3 Funções
+
+# Parte 2
+# Paradigmas de programação
+
+# 2.1 Programação orientada ao objeto
+
+# Parte 3, mas peraê
+# Exemplo básico
+
+# 3.1 Classes
+
+# 3.1.1 Objeto ou Instância
+
+# 3.2 Atributos
+
+# 3.3 Métodos
+
+# Parte 4
+# Outro exemplo
